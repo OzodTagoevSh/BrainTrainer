e1f522cc5153341daca48ef0eebc8fe310521be8
ozodtagoev <ozod.tagoev@gmail.com>
2021-07-28 23:39:03 +0500

    Score and number of questions added.

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,31 +13,48 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button goButton;
+    int locationOfCorrectAnswer;
+    TextView resultTextView;
+    TextView scoreTextView;
+    TextView questionTextView;
+    TextView timerTextView;
+    CountDownTimer countDownTimer;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    int score = 0;
+    int numberOfQuestions = 0;
     ArrayList<Integer> answers = new ArrayList<>();
 
     public void start(View view) {
         goButton.setVisibility(View.INVISIBLE);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void chooseAnswer(View view) {
+        String result = "Wrong (:";
+        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            result = "Correct!";
+            score++;
+        }
+        numberOfQuestions++;
+        resultTextView.setText(result);
+        scoreTextView.setText(score + "/" + numberOfQuestions);
+        resultTextView.setVisibility(View.VISIBLE);
+        newQuestion();
+    }
 
-        TextView questionTextView = findViewById(R.id.questionTextView);
-        Button button0 = findViewById(R.id.button0);
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-
+    public void newQuestion() {
         Random random = new Random();
-        goButton = findViewById(R.id.goButton);
         int a = random.nextInt(20) + 1;
         int b = random.nextInt(20) + 1;
 
         questionTextView.setText(a + " + " + b);
 
-        int locationOfCorrectAnswer = random.nextInt(4);
+        locationOfCorrectAnswer = random.nextInt(4);
+
+        answers.clear();
+
         for(int i = 0; i < 4; i++) {
             if(i == locationOfCorrectAnswer) {
                 answers.add(a + b);
@@ -54,5 +72,25 @@ public class MainActivity extends AppCompatActivity {
         button1.setText(Integer.toString(answers.get(1)));
         button2.setText(Integer.toString(answers.get(2)));
         button3.setText(Integer.toString(answers.get(3)));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        questionTextView = findViewById(R.id.questionTextView);
+        resultTextView = findViewById(R.id.resultTextView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        timerTextView = findViewById(R.id.timerTextView);
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        goButton = findViewById(R.id.goButton);
+        newQuestion();
+
+
     }
 }
